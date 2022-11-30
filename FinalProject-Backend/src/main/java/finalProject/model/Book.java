e88432776573f8dad096107b8a05eabdb61e5331
alymo8 <aly.mohamed@mail.mcgill.ca>
2022-11-30 13:45:32 -0500

    Set up DB and model successfully

---
+++ FinalProject-Backend/src/main/java/finalProject/model/Book.java
@@ -0,0 +1,61 @@
+package finalProject.model;
+import javax.persistence.*;
+import java.util.Date;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+public class Book {
+
+    UUID id;
+    String name;
+    String isbn;
+    String author;
+    Date datePublished;
+
+    public Book(){}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getDatePublished() {
+        return datePublished;
+    }
+
+    public void setDatePublished(Date datePublished) {
+        this.datePublished = datePublished;
+    }
+
+}
---
+++ FinalProject-Backend/src/main/java/finalProject/model/User.java
@@ -0,0 +1,100 @@
+package finalProject.model;
+
+import lombok.AllArgsConstructor;
+import javax.persistence.*;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@AllArgsConstructor
+public class User {
+
+    UUID id;
+    String username;
+    String password;
+    String name;
+    String email;
+    String phoneNumber;
+    String address;
+    List<Book> borrowedBooks;
+    List<Book> offeredBooks;
+
+    public User() {}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @OneToMany
+    public List<Book> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    public void setBorrowedBooks(List<Book> borrowedBooks) {
+        this.borrowedBooks = borrowedBooks;
+    }
+
+    @OneToMany
+    public List<Book> getOfferedBooks() {
+        return offeredBooks;
+    }
+
+    public void setOfferedBooks(List<Book> offeredBooks) {
+        this.offeredBooks = offeredBooks;
+    }
+
+}
