a136e8ea7154c44053433cfca63469182d8c6b25
RobertApra <robert.aprahamian@mail.mcgill.ca>
2022-12-04 13:11:56 -0500

    BOOK-10 and BOOK-17 service and controller done

---
+++ FinalProject-Backend/src/main/java/finalProject/controller/BookController.java
@@ -0,0 +1,53 @@
+package finalProject.controller;
+
+
+import finalProject.dto.BookDTO;
+import finalProject.dto.UserDTO;
+import finalProject.model.Book;
+import finalProject.model.User;
+import finalProject.service.BookService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@CrossOrigin(origins = "*")
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/books")
+public class BookController {
+
+    private BookService bookService;
+
+    @PostMapping
+    public ResponseEntity<?> createBook(@RequestBody BookDTO bookDTO) {
+        Book book;
+        try {
+            book = bookService.createBook(bookDTO);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(Conversion.convertToDTO(book), HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/{id}")
+    public BookDTO getBook(@PathVariable("id") UUID id) {
+        return Conversion.convertToDTO(bookService.getBookById(id));
+    }
+
+    @DeleteMapping(value = {"/{id}"})
+    public boolean deleteBook(@PathVariable("id") UUID id) {
+        return bookService.deleteBook(id);
+    }
+
+    @GetMapping
+    public List<BookDTO> viewBooks(){
+        return bookService.getAllBooks().stream()
+                .map(Conversion::convertToDTO)
+                .collect(Collectors.toList());
+    }
+}
---
+++ FinalProject-Backend/src/main/java/finalProject/service/BookService.java
@@ -0,0 +1,60 @@
+package finalProject.service;
+
+import finalProject.dao.BookRepository;
+import finalProject.dao.UserRepository;
+import finalProject.dto.*;
+import finalProject.model.*;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+@Transactional
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    public Book createBook(BookDTO bookDTO) {
+        Book book = new Book();
+        String isbn = bookDTO.getIsbn();
+        String author = bookDTO.getAuthor();
+        Date datePublished = bookDTO.getDatePublished();
+        String name = bookDTO.getName();
+
+        if(name == null || name.equals("")) throw new IllegalArgumentException("Name cannot be blank");
+        if(isbn == null || isbn.equals("")) throw new IllegalArgumentException("Isbn cannot be blank");
+        if(author == null || author.equals("")) throw new IllegalArgumentException("Author cannot be blank");
+        if(datePublished == null) throw new IllegalArgumentException("Date cannot be blank");
+
+        book.setAuthor(author);
+        book.setName(name);
+        book.setDatePublished(datePublished);
+        book.setIsbn(isbn);
+
+        return bookRepository.save(book);
+    }
+
+    public Book getBookById(UUID id) {
+        return bookRepository.findById(id).orElse(null);
+    }
+
+    public boolean deleteBook(UUID id) {
+        try {
+            bookRepository.deleteById(id);
+            return true;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+
+}
