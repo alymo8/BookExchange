ff598f0087903d4b86c253d9ad52ea0e14a58ddc
RobertApra <robert.aprahamian@mail.mcgill.ca>
2022-12-05 20:11:34 -0500

    small changes

--- finalproject-frontend/src/components/BookCard.js
+++ finalproject-frontend/src/components/BookCard.js
@@ -1,5 +1,20 @@
 import React from "react";
-import {Card, CardHeader, CardContent, Typography} from "@mui/material";
+import {Card, CardHeader, CardContent, Typography, Button} from "@mui/material";
+
+const loggedIn = (() => {
+    console.log("user", localStorage.getItem("userId"))
+    if (localStorage.getItem("userId") != null) {
+        return true;
+    } else {
+        return false
+    }
+})();
+
+// const handleBorrowBook = React.useCallback(() => {
+//     console.log("Offer book button pressed")
+//     window.location.href = "offerBook";
+// }, [])
+
 const BookCard = (props) => {
     return (
         <div style={{ marginTop:20, marginRight:80, marginLeft:80, color:'gray'}}>
@@ -15,7 +30,8 @@ const BookCard = (props) => {
                 <CardContent>
                     <Typography variant="body2" color="text.secondary">
                         {"isbn: " + props.book.isbn} <br/>
-                        {"Availability: " + props.book.available} <br/>
+                        {"Availability: " + props.book.available}
+                        {loggedIn && <Button style={{display: 'flex', justifyContent:'flex-end', alignItems:"flex-end"}}>Borrow book</Button>}
                     </Typography>
                 </CardContent>
             </Card>
